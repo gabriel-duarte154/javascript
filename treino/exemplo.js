d94fda495039a7b1d8c2d62c4a07a9f3c0b9d082
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-03-29 07:07:01 -0300

    Exercícos js

---
+++ treino/exemplo.js
@@ -0,0 +1,14 @@
+
+ let valor = -9;
+function operacao(num) {
+
+    if (valor >= 0) {
+
+        return Math.sqrt(num);
+    } else {
+
+        return num * num;
+    };
+
+}; 
+console.log(operacao(valor));
---
+++ treino/node_f5d2c88c4479a.tmp.js
@@ -0,0 +1,32 @@
+//declaração de vaiáveis
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+let valorCompra;
+let lucro;
+let valorFinal;
+
+
+//fim
+
+//Área de entrada de valores
+
+
+//Fim
+
+// Tratamento e saída de dados
+
+if (valorCompra < 20 ) {
+
+    lucro = valorCompra * 0.45;
+    valorFinal = lucro + valorCompra;
+} else {
+
+    lucro = valorCompra * 0.30;
+    valorFinal = lucro + valorCompra;
+};
+
+
+console.log(valorFinal.toLocaleString("pt-br", {style: 'currency', currency:"BRL"}));
