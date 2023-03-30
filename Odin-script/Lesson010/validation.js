2d34fb6ed4b40905593565c0bf37e43d52d2e1dd
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-03-30 15:10:35 -0300

    Regex formulário

--- Odin-script/Lesson010/validation.js
+++ Odin-script/Lesson010/validation.js
@@ -1,23 +1,26 @@
-const inputs = document.querySelectorAll('input')
+const inputs = document.querySelectorAll('input');
 
 const patterns = {
     telephone: /^\d{2}\s(9)\s\d{4}\s\d{4}$/,
-    username: /^\w{5,12}$/,
-    password: /^\w{8,20}$/,
-    email: /\w{2,20}@\w{2,10}.(com|me|io)/
-
-}
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/,
+    email: /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+    profileSlug: /^[a-z\d-]{8,20}$/
+};
 
 function validate(fild, regex) {
     if (regex.test(fild.value)) {
         fild.className = "valid"
     } else {
         fild.className = "invalid"
+    };
+    if (fild.name.value = null) {
+        fild.className = ""
     }
-}
+};
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', (e) => {
        validate(e.target, patterns[e.target.attributes.name.value])
-    })
-})
+    });
+});
