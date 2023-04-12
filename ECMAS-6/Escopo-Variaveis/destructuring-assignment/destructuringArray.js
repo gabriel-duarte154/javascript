a51bb6247ba88396fd37b4f742a79d32e304da69
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-04-12 16:31:13 -0300

    destructuring estudos

---
+++ ECMAS-6/Escopo-Variaveis/destructuring-assignment/destructuringArray.js
@@ -0,0 +1,24 @@
+//Podemos usar destructureing em array tambem
+
+let a = 10;
+let b = 7;
+
+[a, b] = [b, a];
+
+console.log(a);
+console.log(b);
+
+//simplesmente invertemos os valores
+
+let myNums = [4, 8, 5, 6];
+let [fisrt, second] = myNums;
+console.log(fisrt, second);
+
+//As vezes queremos guardar o os outrso números 
+
+let alphabelt = ["a", "b", "c", "d", "e", "f", "g"];
+let [,, ...rest] = alphabelt;
+
+console.log(rest);
+
+console.log(alphabelt.slice(2, ))
