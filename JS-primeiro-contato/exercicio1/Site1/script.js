5993ecd31ec379ded68af3370f54f61ec728d649
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-04-18 10:49:24 -0300

    Mudança de estilo Site1

--- JS-primeiro-contato/exercicio1/Site1/script.js
+++ JS-primeiro-contato/exercicio1/Site1/script.js
@@ -3,24 +3,24 @@ function carregar(){
     var img = document.querySelector('img#imagem')
     var saud = document.querySelector('div#saudacao')
     var data = new Date()
-    var hora = data.getHours()
-    var min = data.getUTCMinutes()
+    var hora = data.getHours();
+    var min = data.getMinutes();
     msg.innerHTML = `Agora são ${hora}:${min}`
     if (hora >= 0 && hora < 12){
         //bom dia
         img.src = 'fotomanha.jpg'
-        document.body.style.background ="#0a81d5"
+        document.body.style.cssText = 'background-image: linear-gradient(225deg, #0082DA, #EBF8FE)'
+        document.body.style.color = "black"
         saud.innerHTML ="Bom dia!"
     }else if(hora >= 12 && hora < 18){
         //boa tarde
         img.src = 'fototarde.jpg'
-        document.body.style.background = "#fd6f2e"
+        document.body.style.cssText = 'background-image: linear-gradient(#FEA001, #D03300)'
         saud.innerHTML ="Boa tarde!" 
         } else{
         //boa noite
         img.src = 'fotonoite.jpg' 
-        document.body.style.background ="#1c3e5f"  
+        document.body.style.cssText = 'background-image: linear-gradient( #23497A, #1F5264)' 
         saud.innerHTML ="Boa noite!" 
     }
-
 }
